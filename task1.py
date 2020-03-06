5cdbf590e2f2cb83a8a2470d09fa903dacc806bb
uaArsen <arsenzhd@gmail.com>
2020-03-06 21:37:25 +0200

    Done

---
+++ task1.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    ingredients = ["hleb", "pomidor", "mascorpone", "mayanez"]
+    i = 0
+    while i != len(ingredients):
+        ingredient = ingredients[i]
+        i += 1
+        print(ingredient)
+
+    for ingredient in ingredients:
+        print(ingredient)
